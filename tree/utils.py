5498ccff8d9ecd65ce23c01e75308da36f15ecc7
Kommanapalli Hemanth <kommanapalli.hemanth@iitgn.ac.in>
2025-08-27 22:52:44 +0530

    Update utils.py

--- tree/utils.py
+++ tree/utils.py
@@ -1,77 +1,108 @@
-"""
-You can add your own functions here according to your decision tree implementation.
-There is no restriction on following the below template, these fucntions are here to simply help you.
-"""
-
+import math 
 import pandas as pd
+import numpy as np
 
-def one_hot_encoding(X: pd.DataFrame) -> pd.DataFrame:
-    """
-    Function to perform one hot encoding on the input data
-    """
+def entropy(Y):
+    temp = np.unique(Y, return_counts=True)
+    uniq_Y = list(temp[0])
+    Y_count = list(temp[1])
+    total = sum(Y_count)
+    ent = 0
+    for elem in uniq_Y:
+        prob = Y_count[uniq_Y.index(elem)] / total
+        ent -= (prob * (math.log2(prob)))
+    return ent
 
-    pass
+def information_gain(Y, attr):
+    initial_gain = entropy(Y)
+    temp_Y = Y.tolist()
+    temp_attr = attr.tolist()
+    temp_attr = list(np.unique(attr))
 
-def check_ifreal(y: pd.Series) -> bool:
-    """
-    Function to check if the given series has real or discrete values
-    """
+    for a in temp_attr:
+        l = []
+        count = 0
+        for j in attr:
+            if (j == a):
+                l.append(temp_Y[count])
+            count+=1
+        initial_gain -= ((len(l) / len(temp_Y)) * entropy(pd.Series(l)))
+    return initial_gain
 
-    pass
+def variance(Y):
+    if (isinstance(Y, pd.Series)):
+        Y = Y.tolist()
+    
+    total_samples = len(Y)
+    Y_squares = [i*i for i in Y]
 
+    sq_mean = sum(Y_squares)/total_samples
+    mean_sq = (sum(Y)/total_samples)**2
+    return(sq_mean-mean_sq)
 
-def entropy(Y: pd.Series) -> float:
-    """
-    Function to calculate the entropy
-    """
+def variance_gain(Y, attr):
+    initial_gain = variance(Y)
+    Y = Y.tolist()
+    attr = attr.tolist()
+    attr_set = set(attr)
+    attr_set = list(attr_set)
+    
+    for i in attr_set:
+        l = []
+        for j in range (len(attr)):
+            if attr[j] == i:
+                l.append(Y[j])
+        initial_gain = initial_gain-(len(l)/len(Y))*variance(l)
+    return initial_gain
 
-    pass
+def gini_index(Y):
+    if (isinstance(Y, list) == False):
+        temp_Y = Y.tolist()
+    else:
+        temp_Y = Y
+        
+    total_samples = len(temp_Y)
+    temp = np.unique(Y, return_counts=True)
+    Y_count = list(temp[1])
+    Y_unique = list(temp[0])
 
+    ans = 1
+    for attr in Y_unique:
+        g = Y_count[Y_unique.index(attr)] / total_samples
+        ans -= (g**2)
+    return ans
 
-def gini_index(Y: pd.Series) -> float:
-    """
-    Function to calculate the gini index
-    """
+def gini_gain(Y, attr):
+    Y = Y.tolist()
+    attr = attr.tolist()
+    attr_set = set(attr)
+    attr_set = list(attr_set)
+    initial_gain=0
+    for i in attr_set:
+        l = []
+        for j in range (len(attr)):
+            if attr[j] == i:
+                l.append(Y[j])
+        initial_gain = initial_gain+(len(l)/len(Y))*gini_index(l)
+    return initial_gain
 
-    pass
+def loss(Y, split_index):
+    if (isinstance(Y, list) == False):
+        y = Y.tolist()
+    
+    total_samples = len(y)
+    c1 = 0
+    c2 = 0
+    for i in range(total_samples):
+        if (i <= split_index):
+            c1 += y[i]
+        else:
+            c2 += y[i]
+    c1 /= total_samples
+    c2 /= total_samples
 
-
-def information_gain(Y: pd.Series, attr: pd.Series, criterion: str) -> float:
-    """
-    Function to calculate the information gain using criterion (entropy, gini index or MSE)
-    """
-
-    pass
-
-
-def opt_split_attribute(X: pd.DataFrame, y: pd.Series, criterion, features: pd.Series):
-    """
-    Function to find the optimal attribute to split about.
-    If needed you can split this function into 2, one for discrete and one for real valued features.
-    You can also change the parameters of this function according to your implementation.
-
-    features: pd.Series is a list of all the attributes we have to split upon
-
-    return: attribute to split upon
-    """
-
-    # According to wheather the features are real or discrete valued and the criterion, find the attribute from the features series with the maximum information gain (entropy or varinace based on the type of output) or minimum gini index (discrete output).
-
-    pass
-
-
-def split_data(X: pd.DataFrame, y: pd.Series, attribute, value):
-    """
-    Funtion to split the data according to an attribute.
-    If needed you can split this function into 2, one for discrete and one for real valued features.
-    You can also change the parameters of this function according to your implementation.
-
-    attribute: attribute/feature to split upon
-    value: value of that attribute to split upon
-
-    return: splitted data(Input and output)
-    """
-
-    # Split the data based on a particular value of a particular attribute. You may use masking as a tool to split the data.
-
-    pass
+    loss = 0
+    for i in range(total_samples):
+        loss += ((y[i] - c1)**2 + (y[i]-c2)**2)
+    return loss
+        
